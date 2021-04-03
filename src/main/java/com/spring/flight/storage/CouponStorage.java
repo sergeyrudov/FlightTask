838519f0867766140d257cbe38b5d401533a8f0a
sergeyrudov <extrazeto@gmail.com>
2021-04-03 14:36:58 +0300

    fix formula of discount

--- src/main/java/com/spring/flight/storage/CouponStorage.java
+++ src/main/java/com/spring/flight/storage/CouponStorage.java
@@ -36,6 +36,6 @@ public class CouponStorage {
     public double getNewPrice(CouponDTO couponDTO) {
         Coupon coupon = getCouponById(couponDTO.getId());
 
-        return couponDTO.getPrice() * coupon.getDiscount();
+        return couponDTO.getPrice() - coupon.getDiscount();
     }
 }
